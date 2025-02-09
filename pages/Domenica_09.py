63e07666e384f45285773313704e49c27d58dee0
Jacopo Ghisleni <jacopo.ghisleni@gmail.com>
2025-02-09 18:42:28 +0100

    Update Domenica_09.py

--- pages/Domenica_09.py
+++ pages/Domenica_09.py
@@ -44,7 +44,7 @@ def is_valid_direction_filter(current_pos, candidate_pos, direction,stream):
         if dist_y>dist_x: x=True  
         else: x=False
     elif direction == "orizzontale":
-        if dist_y<dist_x:   x=True   
+        if dist_y<dist_x:   x=False  
         else: x=False
     else: x=True  
     #st.write(f"Condition met? {x}")  # Debugging
