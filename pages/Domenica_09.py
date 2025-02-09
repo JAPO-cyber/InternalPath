2ab2b668ea9487ab3ad16977e0ba520fa5b698bd
Jacopo Ghisleni <jacopo.ghisleni@gmail.com>
2025-02-09 18:48:01 +0100

    Update Domenica_09.py

--- pages/Domenica_09.py
+++ pages/Domenica_09.py
@@ -34,7 +34,7 @@ def is_valid_direction_filter(current_pos, candidate_pos, direction,stream):
     dist_x = abs(x1 - x2)
     dist_y = abs(y1 - y2)
     st.write(f"Current: {current_pos}, Candidate: {candidate_pos}")
-    #st.write(f"dist_x: {dist_x}, dist_y: {dist_y}, direction: {direction}")
+    st.write(f"dist_x: {dist_x}, dist_y: {dist_y}, direction: {direction}")
     x = False
     
     if not isinstance(direction, str):
