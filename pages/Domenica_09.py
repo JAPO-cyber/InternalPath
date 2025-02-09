ec16477d3346564490f8aceabee2d266aa17f480
Jacopo Ghisleni <jacopo.ghisleni@gmail.com>
2025-02-09 11:24:05 +0100

    Update Domenica_09.py

--- pages/Domenica_09.py
+++ pages/Domenica_09.py
@@ -47,9 +47,8 @@ def is_valid_direction_filter(current_pos, candidate_pos, direction,stream):
             else: return False    
         else:
             return False
-    elif direction == "orizzontale":
-    else:
-            return False 
+    elif direction == "orizzontale": return False 
+    else: return False 
 def breakdown_path(path, pos):
     """
     Data una lista di nodi (path) e il dizionario pos,
